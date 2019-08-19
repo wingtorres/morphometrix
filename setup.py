7e3f25c2ed4cc6a0ec8b2f0ca47e4f1f45c19b30
wingtorres <walter.torres@duke.edu>
2019-08-19 02:44:32 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="morphometrix",
+    version="1.0.0",
+    author="Walter Torres",
+    author_email="walter.torres@duke.edu",
+    url = "https://github.com/wingtorres/morphometrix/",
+    packages= find_packages()
+)
